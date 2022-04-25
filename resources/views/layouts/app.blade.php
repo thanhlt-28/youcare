71386e2516e702414f4689100643c582f5ee1571
Tien Thanh <59819766+thanhlt-28@users.noreply.github.com>
2022-04-25 17:17:19 +0700

    update app

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,123 @@
+<div class="container">
+    <!--==================Hệ sinh thái==================-->
+    <div class="row">
+        <!-- <div class="col-lg-6 video-box align-self-baseline position-relative">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+            <a target="_blank" href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox play-btn mb-4"></a>
+          </div> -->
+        <div class="row justify-content-end">
+            <div class="col-lg-12">
+                <div class="row justify-content-end">
+                    <div class="col-lg-4 col-md-5 col-6 d-md-flex align-items-md-stretch"></div>
+
+                    <div style="margin-bottom: -50px;" class="col-lg-4 col-md-5 col-6 d-md-flex align-items-md-stretch">
+                        <a style=" color:#5a5454;" class="details text-start" target="_blank" href="{{route('clients.platform.chkcare_detail')}}">
+                            <div class="box-shade count-box py-5">
+                                <h4 class="text-center text-uppercase"> CHKCARE</h4>
+                                <div class="d-flex flex-nowrap">
+                                    <p class="box-shade text-justify">
+                                        ChkCare là sản phẩm công nghệ
+                                        được ứng dụng trong việc theo dõi,
+                                        sàng lọc và đánh giá nhằm phát
+                                        hiện sớm những vấn đề rối loạn
+                                        phát triển cũng như sức khỏe
+                                        tâm thần của trẻ từ 12 tháng
+                                        tuổi đến 18 tuổi.
+                                    </p>
+                                </div>
+                                <p class="text-white">Chi tiết<span class='bi bi-arrow-right'></span></p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-5 col-6 d-md-flex align-items-md-stretch"></div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-end">
+            <div style="margin-left:20px" class="col-lg-12">
+                <div class="row justify-content-end">
+
+                    <div class="col-lg-4 col-md-5 col-6 d-flex align-items-center">
+                        <a style=" color:#5a5454;" class="details text-start" href="#">
+                            <div class="box-shade count-box pb-5 pt-0 pt-lg-5">
+                                <h4 class="text-center text-uppercase"> PUBCARE</h4>
+                                <div class="d-flex flex-nowrap text-end">
+                                    <p class="box-shade text-justify">
+                                        PubCare là sản phẩm công nghệ được
+                                        ứng dụng vào việc sàng lọc miễn phí
+                                        sức khỏe cộng đồng. Giúp những người gặp phải
+                                        vấn đề về tâm lý và
+                                        sức khỏe tâm thần biết được vấn đề
+                                        mình đang gặp phải. Sản phẩm miễn
+                                        phí cho cộng đồng.
+                                    </p>
+                                </div>
+                                <p class="text-white">Chi tiết<span class='bi bi-arrow-right'></span></p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-5 col-6 align-self-center">
+                        <div class="count-box py-5">
+                            <img class="rounded mx-auto d-block" src="assets/img/hesinhthai.png" width="100%" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-5 col-6 d-flex align-items-center">
+                        <a style=" color:#5a5454;" class="details text-start" href="#">
+                            <div class="box-shade count-box pb-5 pt-0 pt-lg-5">
+                                <!-- <i class="bi bi-award"></i> -->
+                                <!-- <span data-purecounter-start="0" data-purecounter-end="3" class="purecounter">3</span> -->
+                                <h4 class="text-center text-uppercase"> PSYCARE</h4>
+                                <div class="d-flex flex-nowrap">
+                                    <p class="box-shade text-justify">
+                                        PsyCare là nền tảng tham vấn và trị liệu
+                                        các vấn đề về tâm lý và sức khỏe tâm
+                                        thần online. Được kết nối với các chuyên
+                                        gia tâm lý và chuyên gia sức khỏe tâm
+                                        thần hàng đầu.
+                                    </p>
+
+                                </div>
+                                <p class="text-white">Chi tiết<span class='bi bi-arrow-right'></span></p>
+                            </div>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!--==================4==================-->
+        <div style="margin-top: -50px" class="row justify-content-end">
+            <div class="col-lg-12">
+                <div class="row justify-content-end">
+
+                    <div class="col-lg-4 col-md-5 col-6 d-md-flex align-items-md-stretch"></div>
+                    <div class="col-lg-4 col-md-5 col-6 align-self-center">
+                        <a style=" color:#5a5454;" class="details text-start" href="#">
+                            <div class="box-shade count-box py-5">
+                                <!-- <i class="bi bi-journal-richtext"></i> -->
+                                <!-- <span data-purecounter-start="0" data-purecounter-end="4" class="purecounter">4</span> -->
+                                <h4 class="text-center text-uppercase"> BIZCARE</h4>
+                                <div class="d-flex flex-nowrap">
+                                    <p class="box-shade text-justify">
+                                        BizCare là sản phẩm công nghệ được
+                                        ứng dụng vào doanh nghiệp để theo
+                                        dõi và chăm sóc sức khỏe tỉnh thần
+                                        nhân sự. Giúp môi môi trường,
+                                        doanh nghiệp vui vẻ hạnh phúc và
+                                        nâng cao hiệu quả công việc.
+                                    </p>
+                                </div>
+                                <p class="text-white">Chi tiết<span class='bi bi-arrow-right'></span></p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-5 col-6 d-md-flex align-items-md-stretch"></div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
