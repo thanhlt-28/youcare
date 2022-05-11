943df3f6574795415e6e88b80ba4c6da50af5014
Tien Thanh <lothanh28.tl@gmail.com>
2022-05-12 00:11:53 +0700

    commit style mobile

--- resources/views/layouts/ck_detail/ck_tabs.blade.php
+++ resources/views/layouts/ck_detail/ck_tabs.blade.php
@@ -51,7 +51,7 @@
                     </div>
                     <div class="tab-pane fade pt-4 me-4" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
                         <h4>Phụ huynh</h4>
-                        <p> Dễ dàng theo dõi và đánh giá được sức khoẻ tâm thần của con mà không cần kiến thức chuyên môn  chỉ bằng click chuột.</p>
+                        <p> Dễ dàng theo dõi và đánh giá được sức khoẻ tâm thần của con mà không cần kiến thức chuyên môn chỉ bằng click chuột.</p>
                         <p> Sớm phát hiện vấn đề của con gặp phải để có phương án hỗ trợ cho con.</p>
                         <p> Được đội ngũ chuyên gia nhiều năm kinh nghiệm hỗ trợ và đồng hành cùng con khi cần thiết.</p>
                         <p> Các bộ công cụ đã được sử dụng nhiều nước trên thế giới và được chuẩn hóa phù hợp với người Việt Nam.</p>
@@ -60,10 +60,9 @@
                         <h4>Học sinh</h4>
                         <p>Biết được tình trạng của mình và tìm kiếm sự hỗ trợ khi cần thiết chỉ đơn giản bằng lick chuột.</p>
                         <p>Dễ dàng kiểm tra sức khoẻ tâm thần của mình mà không cần kiến thức chuyên môn.</p>
-                        <p>Được đội ngũ chuyên gia nhiều năm kinh nghiệm hỗ trợ và đồng hành cùng  khi cần hỗ trợ.</p>
+                        <p>Được đội ngũ chuyên gia nhiều năm kinh nghiệm hỗ trợ và đồng hành cùng khi cần hỗ trợ.</p>
                     </div>
                     <div class="tab-pane fade pt-4 me-4" id="v-pills-teams" role="tabpanel" aria-labelledby="v-pills-teams-tab">
-                        <h4>Nhà chuyên môn</h4>
                         <p>Là hệ thống đầy đủ các bộ công cụ theo dõi, đánh giá, sàng lọc được tính toán xử lý tự động.</p>
                         <p>Các bộ công cụ đã được sử dụng nhiều nước trên thế giới và được chuẩn hóa phù hợp với người Việt Nam. Nhà chuyên môn không phải tập huấn sử dụng bộ công cụ. Kết quả chính xác và quản lý trẻ em, thân chủ đơn giản và thuận tiện.</p>
                         <p>Được hỗ trợ của các chuyên gia đầu ngành về sức khỏe tinh thần.</p>
@@ -71,5 +70,99 @@
                 </div>
             </div>
         </div>
+    </div>
+</section>
+
+<section class="according-content" data-aos="fade-up">
+    <div class="col-lg-12">
+        <div class="card-body">
+            <!-- Default Accordion -->
+            <div class="accordion" id="accordionExample">
+                
+                <div class="accordion-item">
+                    <h2 style="font-weight: bold; font-size: 18px" class="accordion-header" id="teacher">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTeacher" aria-expanded="true" aria-controls="collapseTeacher">
+                            Giáo viên
+                        </button>
+                    </h2>
+                    <div id="collapseTeacher" class="accordion-collapse collapse show" aria-labelledby="teacher" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <p>Phát hiện sớm những học sinh có những vấn đề về sức khoẻ tinh thần để thông báo cho phụ huynh kịp thời.
+                                Cho kết quả nhanh và chính xác</p>
+                            <p>Dễ dàng theo dõi sự phát triển của trẻ em nhằm phát hiện sớm những vấn đề giúp cho học sinh được hỗ trợ kịp thời.</p>
+                            <p>Không cần phải đi tập huấn, đào tạo về sử dụng công cụ đánh giá. Đơn giản chỉ bằng click chuột.</p>
+                            <p>Các bộ công cụ đã được sử dụng nhiều nước trên thế giới và được chuẩn hóa phù hợp với người Việt Nam.</p>
+                            <p>Dễ dàng quản lý tình trạng sức khoẻ tâm thần của học sinh trong lớp mà không cần kiến thức chuyên môn.</p>
+                            <p>Được hỗ trợ của các chuyên gia đầu ngành về sức khỏe tinh thần khi học sinh gặp phải vấn đề.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="parent">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseParent" aria-expanded="false" aria-controls="collapseParent">
+                            Phụ huynh
+                        </button>
+                    </h2>
+                    <div id="collapseParent" class="accordion-collapse collapse" aria-labelledby="parent" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <p> Dễ dàng theo dõi và đánh giá được sức khoẻ tâm thần của con mà không cần kiến thức chuyên môn chỉ bằng click chuột.</p>
+                            <p> Sớm phát hiện vấn đề của con gặp phải để có phương án hỗ trợ cho con.</p>
+                            <p> Được đội ngũ chuyên gia nhiều năm kinh nghiệm hỗ trợ và đồng hành cùng con khi cần thiết.</p>
+                            <p> Các bộ công cụ đã được sử dụng nhiều nước trên thế giới và được chuẩn hóa phù hợp với người Việt Nam.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="student">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseStudent" aria-expanded="false" aria-controls="collapseStudent">
+                            Học sinh
+                        </button>
+                    </h2>
+                    <div id="collapseStudent" class="accordion-collapse collapse" aria-labelledby="student" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <p>Biết được tình trạng của mình và tìm kiếm sự hỗ trợ khi cần thiết chỉ đơn giản bằng lick chuột.</p>
+                            <p>Dễ dàng kiểm tra sức khoẻ tâm thần của mình mà không cần kiến thức chuyên môn.</p>
+                            <p>Được đội ngũ chuyên gia nhiều năm kinh nghiệm hỗ trợ và đồng hành cùng khi cần hỗ trợ.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="special">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSpecial" aria-expanded="false" aria-controls="collapseSpecial">
+                            Nhà chuyên môn
+                        </button>
+                    </h2>
+                    <div id="collapseSpecial" class="accordion-collapse collapse" aria-labelledby="special" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <p>Là hệ thống đầy đủ các bộ công cụ theo dõi, đánh giá, sàng lọc được tính toán xử lý tự động.</p>
+                            <p>Các bộ công cụ đã được sử dụng nhiều nước trên thế giới và được chuẩn hóa phù hợp với người Việt Nam. Nhà chuyên môn không phải tập huấn sử dụng bộ công cụ. Kết quả chính xác và quản lý trẻ em, thân chủ đơn giản và thuận tiện.</p>
+                            <p>Được hỗ trợ của các chuyên gia đầu ngành về sức khỏe tinh thần.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <h2 class="accordion-header" id="manager">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseManager" aria-expanded="false" aria-controls="collapseManager">
+                             Nhà quản lý
+                        </button>
+                    </h2>
+                    <div id="collapseManager" class="accordion-collapse collapse" aria-labelledby="manager" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <p>Dễ dàng theo dõi sự phát triển của học sinh trong trường học nhằm phát hiện sớm những vấn đề giúp cho học sinh được phòng ngừa và can thiệp, trị liệu sớm nhất và hiệu quả tốt nhất.</p>
+                            <p>Tiết kiệm thời gian và tài chính không cần phải cứ giáo viên đi học sử dụng bộ công cụ.</p>
+
+                            <p>Ứng dụng được xây dựng trên nền tảng công nghệ hiện đại giúp việc theo dõi và sàng lọc vấn đề của trẻ cho kết quả chính xác và nhanh chóng.</p>
+                            <p>Các bộ công cụ đã được sử dụng nhiều nước trên thế giới và được chuẩn hóa phù hợp với người Việt Nam.</p>
+                            <p>Có thể sàng lọc và đánh giá cho 5000 học sinh trong thời gian 10 phút cho kết quả phân tích thống kê của cả trường học. Dễ dàng tra cứu, quản lý các đợt đánh giá, kết quả đánh giá đến từng học sinh</p>
+                            <p>Hệ thống tự động gửi email cho phụ huynh kết quả đánh giá sàng lọc.</p>
+                            <p>Từ kết quả đánh giá sẽ xây dựng các chương trình ngoại khóa phù hợp với vấn đề của học sinh để phòng ngừa các rối loạn như lo lâu, trầm cảm, rối loạn cảm xúc, chống đôi, tự tự, xâm hại...</p>
+                            <p> Được sự hỗ trợ của các chuyên gia đầu ngành về sức khỏe tâm thần của học sinh khi gặp phải vấn đề.</p>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- End Default Accordion Example -->
+
+        </div>
+
     </div>
 </section>
