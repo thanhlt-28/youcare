a9816812737ebba1d8e4932aca56e0b64365a688
Tien Thanh <59819766+thanhlt-28@users.noreply.github.com>
2022-06-25 12:00:49 +0700

    commit

--- resources/views/clients/platform/product.blade.php
+++ resources/views/clients/platform/product.blade.php
@@ -219,7 +219,7 @@
                     <a href="https://checkingcare.vn/m-chart/" target="_blank">
                         <div class="icon-box">
                             <i class="bi bi-paint-bucket" style="color: #e361ff;"></i>
-                            <h3>M-CHAT-R</h3>
+                            <h3>M-CHAT</h3>
                         </div>
                     </a>
                 </div>
