87f974fc9b4db4343839f0faa930324973381deb
Tien Thanh <lothanh28.tl@gmail.com>
2022-08-29 17:24:08 +0700

    commit home

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
         <div class="hero-content">
           <h2 class="hero-title">NỀN TẢNG CHĂM SÓC SỨC KHỎE TINH THẦN TOÀN DIỆN</h2>
-          <p class="hero-left">YouCare ứng dụng công nghệ vào việc theo dõi, sàng lọc, đánh giá, trị liệu và chăm sóc sức khỏe tinh thần cho cộng đồng. YouCare ứng dụng công nghệ để lan tỏa những trí tuệ của các chuyên gia <br /> hàng đầu trong lĩnh vực chăm sóc sức khỏe tinh thần.</p>
+          <p class="hero-left">YouCare ứng dụng công nghệ vào việc theo dõi, sàng lọc, đánh giá, trị liệu và chăm sóc sức khỏe tinh thần cho cộng đồng. YouCare ứng dụng công nghệ để lan tỏa những trí tuệ của các chuyên gia hàng đầu trong lĩnh vực chăm sóc sức khỏe tinh thần.</p>
 
           <div class="d-flex align-items-center" data-aos="" data-aos-duration="100">
             <a href="{{route('clients.platform.product')}}" class="btn-get-started scrollto">Tìm hiểu ngay!</a>
@@ -24,20 +24,39 @@
       </div>
       <div class="col-lg-6">
         <div class="hero-right">
+          <div class="hero-image__wrapper">
+            <img class="img-hero" src="{{asset('assets/img/svg/people.svg')}}" alt="">
+            <div class="hero-f-1">
+              <a href="{{route('clients.platform.ckcare_detail')}}"> <span style="top: 40px; position: absolute; left: -6%;" class="hero-image-text">CKCare</span><img class="btn-show btn-abc" src="{{asset('assets/img/svg/button.svg')}}" alt=""></a>
+            </div>
+            <div class="hero-f-2">
+              <a href="{{route('clients.platform.bizcare')}}"> <span style="position: absolute; left: 104%; top: 40px;" class="hero-image-text">BizCare</span><img class="btn-show btn-abc" src="{{asset('assets/img/svg/button.svg')}}" alt=""></a>
+            </div>
+            <div class="hero-f-3">
+              <a href="{{route('clients.platform.pubcare')}}"> <span style="top: 310px; left: -5%; position: absolute;" class="hero-image-text">PubCare</span><img class="btn-show btn-abc" src="{{asset('assets/img/svg/button.svg')}}" alt=""></a>
+            </div>
+            <div class="hero-f-4">
+              <a href="{{route('clients.platform.psycare')}}"> <span style="top: 310px; left: 104%; position: absolute;" class="hero-image-text">PsyCare</span><img class="btn-show btn-abc" src="{{asset('assets/img/svg/button.svg')}}" alt=""></a>
+            </div>
+          </div>
+        </div>
+
+        <!-- desktop -->
+        <div class="hero-right">
           <div class="hero-image-wrap">
-          <img class="img-hero" src="{{asset('assets/img/svg/people.svg')}}" alt="">
-          <div class="hero-f-1 ">
-            <a href="#"> <span class="hero-image-text">CKCare</span><img class="btn-show btn-abc" src="{{asset('assets/img/svg/button.svg')}}" alt=""></a>
-          </div>
-          <div class="hero-f-2  ">
-            <a href="#"> <span class="hero-image-text">BizCare</span><img class="btn-show btn-abc" src="{{asset('assets/img/svg/button.svg')}}" alt=""></a>
-          </div>
-          <div class="hero-f-3  ">
-            <a href="#"> <span class="hero-image-text">PubCare</span><img class="btn-show btn-abc" src="{{asset('assets/img/svg/button.svg')}}" alt=""></a>
-          </div>
-          <div class="hero-f-4 ">
-            <a href="#"> <span class="hero-image-text">PsyCare</span><img class="btn-show btn-abc" src="{{asset('assets/img/svg/button.svg')}}" alt=""></a>
-          </div>
+            <img class="img-hero" src="{{asset('assets/img/svg/people.svg')}}" alt="">
+            <div class="hero-f-1">
+              <a href="{{route('clients.platform.ckcare_detail')}}"> <span style="top: 40px; position: absolute; left: 0%;" class="hero-image-text">CKCare</span><img class="btn-show btn-abc" src="{{asset('assets/img/svg/button.svg')}}" alt=""></a>
+            </div>
+            <div class="hero-f-2">
+              <a href="{{route('clients.platform.bizcare')}}"> <span style="position: absolute; left: 111%; top: 40px;" class="hero-image-text">BizCare</span><img class="btn-show btn-abc" src="{{asset('assets/img/svg/button.svg')}}" alt=""></a>
+            </div>
+            <div class="hero-f-3">
+              <a href="{{route('clients.platform.pubcare')}}"> <span style="top: 335px; left: 0%; position: absolute;" class="hero-image-text">PubCare</span><img class="btn-show btn-abc" src="{{asset('assets/img/svg/button.svg')}}" alt=""></a>
+            </div>
+            <div class="hero-f-4">
+              <a href="{{route('clients.platform.psycare')}}"> <span style="top: 335px; left: 111%; position: absolute;" class="hero-image-text">PsyCare</span><img class="btn-show btn-abc" src="{{asset('assets/img/svg/button.svg')}}" alt=""></a>
+            </div>
           </div>
         </div>
       </div>
@@ -46,19 +65,19 @@
   </div>
 
 </section>
-<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 
 <script>
-    let abc =$('.btn-abc');
-   [...abc].forEach(item=>{
-      // console.log(item);
-      $(item).mouseover(()=>{
-        $(item).attr('src','/assets/img/svg/btn-hover.svg')
-      })
-      $(item).mouseout(()=>{
-        $(item).attr('src','assets/img/svg/button.svg')
-      })
+  let abc = $('.btn-abc');
+  [...abc].forEach(item => {
+    // console.log(item);
+    $(item).mouseover(() => {
+      $(item).attr('src', '/assets/img/svg/btn-hover.svg')
     })
+    $(item).mouseout(() => {
+      $(item).attr('src', 'assets/img/svg/button.svg')
+    })
+  })
 </script>
 
 <!-- End Hero -->
