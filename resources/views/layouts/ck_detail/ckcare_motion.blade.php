572f5e4806f7aee4eb7cb18a44a75aa352b8382f
Tien Thanh <lothanh28.tl@gmail.com>
2022-10-19 13:54:43 +0700

    remove code

--- resources/views/layouts/ck_detail/ckcare_motion.blade.php
+++ resources/views/layouts/ck_detail/ckcare_motion.blade.php
@@ -45,22 +45,6 @@
                     </defs>
                 </svg>
             </div>
-            <!-- <div class="row">
-                <div class="value_desc d-flex justify-content-center">
-                    <div class="col-md-2">
-                        <a href="#"><img class="img_value_desc" src="{{asset('assets/img/svg/sang-loc-theo-doi.svg')}}"></a>
-                    </div>
-                    <div class="col-md-2">
-                        <a href="#"><img class="img_value_desc" src="{{asset('assets/img/svg/danh-gia-chuyen-sau.svg')}}"></a>
-                    </div>
-                    <div class="col-md-2">
-                        <a href="#"><img class="img_value_desc" src="{{asset('assets/img/svg/can-thiep-tri-lieu.svg')}}"></a>
-                    </div>
-                    <div class="col-md-2">
-                        <a href="#"><img class="img_value_desc" src="{{asset('assets/img/svg/ho-tro-dong-hanh.svg')}}"></a>
-                    </div>
-                </div>
-            </div> -->
         </div>
 
     </div>
@@ -153,32 +137,6 @@
                     </filter>
                 </defs>
             </svg>
-            <!-- <div class="row row-cols-2 row-cols-md-2 g-1">
-            <div class="col-md-12">
-                <div class="value_number d-flex justify-content-start">
-                    <img class="img_value_number" src="{{asset('assets/img/svg/mobile/gp-ckcare.svg')}}" alt="">
-                    <img class="img_value_icon" src="{{asset('assets/img/svg/mobile/gp-ckcare-01.svg')}}" alt="">
-
-                </div>
-            </div>
-
-            <div class="col-md-12">
-                <div class="value_desc">
-
-                    <a href="#"><img class="img_value" src="{{asset('assets/img/svg/mobile/sang-loc-theo-doi.svg')}}"></a>
-
-
-                    <a href="#"><img class="img_value" src="{{asset('assets/img/svg/mobile/danh-gia-chuyen-sau.svg')}}"></a>
-
-
-                    <a href="#"><img class="img_value" src="{{asset('assets/img/svg/mobile/can-thiep-tri-lieu.svg')}}"></a>
-
-
-                    <a href="#"><img class="img_value" src="{{asset('assets/img/svg/mobile/ho-tro-dong-hanh.svg')}}"></a>
-
-                </div>
-            </div>
-        </div> -->
         </div>
     </div>
 </div>
